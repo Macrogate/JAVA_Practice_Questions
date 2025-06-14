ab5f82a9b6b1c49e77066428236b945f37593cb1
Sumit <sumit.aiml2005@gmail.com>
2025-06-14 09:29:25 +0530

    fahrenheit to celcius

---
+++ Que20.java
@@ -0,0 +1,21 @@
+// Question 20
+// CP - Write a TemperaturConversion program, given the temperature in Fahrenheit as input outputs thetemperature in Celsius
+// // Hint =>
+// 1. Create a fahrenheit variable and take the user's input
+// 2. User the formulae to convert Fahrenheit to Celsius: (°F - 32) x 5/9 = °C and assign the result to
+// celsiusResult  and print the result..
+// I/P => fahrenheit
+// O/P => The ____ fahrenheit is _____ 
+
+
+import java.util.Scanner;
+public class Que20 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int tempInFahrenheit = sc.nextInt();
+        double tempInCelcius = (tempInFahrenheit - 32) * 5.0 / 9.0;
+        System.out.println("The " + tempInFahrenheit + " celcius is " + tempInCelcius + " fahrenheit");
+        sc.close();
+    }
+    
+}
