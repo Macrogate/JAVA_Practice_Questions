5ceab45eba9863982af0429d1e5f16f71bfc72cc
Sumit <sumit.aiml2005@gmail.com>
2025-06-14 09:42:25 +0530

    find total purchase price

---
+++ Que17.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Que17 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int unitPrice = sc.nextInt();
+        int quantity = sc.nextInt();
+        int totalPrice = unitPrice * quantity;
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity is " + quantity + " and the unitPrice is " + unitPrice);
+        sc.close();
+    }
+    
+}
