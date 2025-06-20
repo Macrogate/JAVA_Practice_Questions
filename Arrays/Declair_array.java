600f94b0f2af692eef7ef861b71d64d5c36eb882
Sumit <sumit.aiml2005@gmail.com>
2025-06-20 13:01:04 +0530

    find positive, negative number

---
+++ Arrays/Declair_array.java
@@ -0,0 +1,11 @@
+// declaration of array
+
+package Arrays;
+
+public class Declair_array {
+    public static void main(String [] args){
+        int [] arr = new int [5];
+        System.out.println(arr);
+    } 
+    
+}
